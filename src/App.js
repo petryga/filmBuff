cecf620bf2a2701bcfc7e361dbd0d5823721d849
Lucas <regazzi58@gmail.com>
2020-12-10 16:55:13 -0500

    added Hash Router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Component } from 'react';
-import { BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import { HashRouter as Router, Route, Link} from 'react-router-dom';
 import Catalogue from './Components/Catalogue.js';
 import MovieDetails from './Components/MovieDetails.js'
 import SearchBar from './Components/SearchBar.js';
